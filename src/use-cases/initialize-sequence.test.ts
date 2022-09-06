506018b10ad651c6a6f6c78d97df151a8e16f554
Ismagilov, Ilshat - Contractor <iismagilov@contractor.ea.com>
2022-09-06 09:37:38 +0300

    Use-case: initialize sequence

---
+++ src/use-cases/initialize-sequence.test.ts
@@ -0,0 +1,32 @@
+import { StepWithLabel } from '../core/data/step';
+import { initializeSequence } from './initialize-sequence';
+
+const createStepsMap = (data: Array<[string, StepWithLabel]>) => new Map(data);
+const createSequences = (data: Array<{ id: string, firstStepId: string }>) => data;
+
+it('should throw if sequence is not found', () => {
+  const sequences = createSequences([]);
+  const stepsMap = createStepsMap([]);
+
+  expect(() => initializeSequence(sequences, stepsMap)('income-sequence')).toThrow();
+});
+
+it('should throw if sequence\'s first step is not found', () => {
+  const sequences = createSequences([{ id: 'income-sequence', firstStepId: 'random' }]);
+  const stepsMap = createStepsMap([]);
+
+  expect(() => initializeSequence(sequences, stepsMap)('income-sequence')).toThrow();
+});
+
+it('should get first step of chosen sequence', () => {
+  const sequenceId = 'income-sequence';
+  const stepId = 'price-step-id';
+  const stepLabel = 'Price label';
+
+  const sequences = createSequences([{ id: sequenceId, firstStepId: stepId }]);
+  const stepsMap = createStepsMap([[stepId, { label: stepLabel }]]);
+
+  const stepInfo = initializeSequence(sequences, stepsMap)('income-sequence');
+
+  expect(stepInfo).toEqual({ id: stepId, label: stepLabel });
+});
---
+++ src/use-cases/initialize-sequence.ts
@@ -0,0 +1,19 @@
+import { StepWithLabel } from '../core/data/step';
+
+export const initializeSequence =
+  (sequences: Array<{ id: string; firstStepId: string }>, stepsMap: Map<string, StepWithLabel>) =>
+  (sequenceId: string) => {
+    const sequence = sequences.find(({ id }) => id === sequenceId);
+
+    if (!sequence) throw new Error(`${sequenceId} sequence is not found!`);
+
+    const stepId = sequence.firstStepId;
+    const stepInfo = stepsMap.get(stepId);
+
+    if (!stepInfo) throw new Error(`Step ${stepId} is not found!`);
+
+    return {
+      id: stepId,
+      label: stepInfo.label,
+    };
+  };
