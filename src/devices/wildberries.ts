4bfec1cd67402a54168a0f2a82e59b030a3dddba
Ismagilov, Ilshat - Contractor <iismagilov@contractor.ea.com>
2022-09-24 12:57:45 +0300

    Monads: refactor wb use cases

--- src/devices/wildberries.ts
+++ src/devices/wildberries.ts
@@ -1,5 +1,9 @@
 import axios from 'axios';
 import { formatRFC3339, startOfToday } from 'date-fns';
+import * as A from 'fp-ts/Array';
+import * as E from 'fp-ts/Either';
+import { pipe } from 'fp-ts/lib/function';
+import * as TE from 'fp-ts/TaskEither';
 
 import { GetOrdersFromWildberries } from '../use-cases/dependencies/wildberries';
 import { WildberriesOrdersResponse } from './data/wildberries-orders-response';
@@ -10,27 +14,31 @@ type WildberriesSDK = {
 
 export const connectToWildberries = (token: string): WildberriesSDK => {
   return {
-    getOrders: async () => {
-      const params = new URLSearchParams({
-        date_start: formatRFC3339(startOfToday()),
-        take: '100',
-        skip: '0',
-      });
-
-      const response = await axios.get<WildberriesOrdersResponse>(
-        `https://suppliers-api.wildberries.ru/api/v2/orders?${params.toString()}`,
-        {
-          headers: { Authorization: token },
-        },
-      );
-
-      return response.data.orders.map((o) => ({
-        id: o.orderId,
-        dateCreated: o.dateCreated,
-        officeAddress: o.officeAddress,
-        currency: 'RUB',
-        price: o.convertedPrice,
-      }));
-    },
+    getOrders: () =>
+      pipe(
+        new URLSearchParams({
+          date_start: formatRFC3339(startOfToday()),
+          take: '100',
+          skip: '0',
+        }).toString(),
+        TE.tryCatchK(
+          (params) =>
+            axios.get<WildberriesOrdersResponse>(
+              `https://suppliers-api.wildberries.ru/api/v2/orders?${params}`,
+              { headers: { Authorization: token } },
+            ),
+          E.toError,
+        ),
+        TE.map((response) => response.data.orders),
+        TE.map(
+          A.map((o) => ({
+            id: o.orderId,
+            dateCreated: o.dateCreated,
+            officeAddress: o.officeAddress,
+            currency: 'RUB',
+            price: o.convertedPrice,
+          })),
+        ),
+      ),
   };
 };
