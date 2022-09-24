7cbd31c21aa969744569ec6082a75de34cc6d4c1
Ismagilov, Ilshat - Contractor <iismagilov@contractor.ea.com>
2022-09-24 17:07:08 +0300

    Monads: in controller

--- src/devices/users.ts
+++ src/devices/users.ts
@@ -1,4 +1,5 @@
 import * as A from 'fp-ts/Array';
+import * as E from 'fp-ts/Either';
 import { pipe } from 'fp-ts/lib/function';
 import * as O from 'fp-ts/Option';
 
@@ -6,18 +7,18 @@ import { GetSheetInfo } from '../use-cases/dependencies/google-sheet';
 import { User } from './data/user';
 
 type UserGateway = {
-  authorize: (userId: string) => User;
+  authorize: (userId: string) => E.Either<Error, User>;
   getAllUsers: () => User[];
   createSheetInfoGetter: (userId: string) => GetSheetInfo;
 };
 
 export const createUserGateway = (users: User[]): UserGateway => ({
   getAllUsers: () => users,
-  authorize: (userId) => {
-    const user = users.find((u) => u.id === userId);
-    if (!user) throw new Error('Authorization failed!');
-    return user;
-  },
+  authorize: (userId) =>
+    pipe(
+      users.find((u) => u.id === userId),
+      E.fromNullable(new Error('Authorization failed!')),
+    ),
   createSheetInfoGetter: (userId) => {
     const user = O.fromNullable(users.find((u) => u.id === userId));
 
